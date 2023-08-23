de67db9fe506a3b1e6fe53468ebca64ed368bf1c
Jason Gonsalves <gonsalvesjason20@gmail.com>
2023-08-23 10:39:25 +0530

    remove unwanted files and folders

--- ruby/exercise1/exercise1.rb
+++
@@ -1,5 +0,0 @@
-def serial_average(string)
-    array = string.split('-')
-    avg = (array[1].to_f + array[2].to_f) / 2
-    return "#{array[0]}-#{sprintf('%.2f', avg).rjust(5, '0')}"
-end
--- ruby/exercise4/exercise4.rb
+++
@@ -1,9 +0,0 @@
-def skip_sports(array, integer)
-    string_array = []
-    array.each_with_index do |sport, index|
-        if index > integer-1
-            string_array.push("#{index}:#{sport}")
-        end
-    end
-    return string_array
-end
